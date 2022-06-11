e46e3c1ddb7952a4666caf74c4fc17ca6698538b
khaifazil <khairal.fazil@gmail.com>
2022-06-12 06:55:18 +0800

    completed channel 4,5

--- 01-exercise/02-channel/04-channel/main.go
+++ 01-exercise/02-channel/04-channel/main.go
@@ -1,20 +1,32 @@
 package main
 
+import "fmt"
+
 // TODO: Implement relaying of message with Channel Direction
 
-func genMsg() {
+func genMsg(ch1 chan<- string) {
 	// send message on ch1
+	ch1 <- "message"
 }
 
-func relayMsg() {
+func relayMsg(ch1 <-chan string, ch2 chan<- string) {
 	// recv message on ch1
+	m := <-ch1
 	// send it on ch2
+	ch2 <- m
 }
 
 func main() {
 	// create ch1 and ch2
+	ch1 := make(chan string)
+	ch2 := make(chan string)
 
-	// spine goroutine genMsg and relayMsg
-
+	// spin goroutine genMsg and relayMsg
+	go genMsg(ch1)
+	go relayMsg(ch1, ch2)
 	// recv message on ch2
+
+	v := <-ch2
+
+	fmt.Println(v)
 }
